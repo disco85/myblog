e01292ddd4fb561aa0145acf296e5bd3426196b6
John Doe <937310@proton.me>
2026-03-14 13:03:49 +0200

    More about diagrams

--- _SRC/clean-published.sh
+++ _SRC/clean-published.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
 
+SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}")"
+SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
+
+rm -rf "$SCRIPT_DIR/../Old"
+rm -rf "$SCRIPT_DIR/../Paradigms"
+rm -rf "$SCRIPT_DIR/../Philosophy"
+rm -rf "$SCRIPT_DIR/../assets"
+rm -f "$SCRIPT_DIR/../index.html"
+rm -f "$SCRIPT_DIR/org/index.org"
